e458caab737c7ed5ef487ebf195dd5ff4d485756
Hiten Soni <hitensoni58@gmail.com>
2024-05-07 20:04:52 +0530

    Initials

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -40,7 +40,7 @@ cart.forEach((cartItem) =>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link">
                     Delete
                   </span>
                 </div>
@@ -116,7 +116,7 @@ cart.forEach((cartItem) =>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -170,3 +170,10 @@ cart.forEach((cartItem) =>{
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+    console.log(productId);
+  });
+});
